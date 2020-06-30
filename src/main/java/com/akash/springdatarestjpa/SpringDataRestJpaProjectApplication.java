ac2bac1612cc2edbcec5d123e58dbe040738376a
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-06-30 13:01:52 +0530

    Initial Commit Spring DATA REST JPA Project

---
+++ src/main/java/com/akash/springdatarestjpa/SpringDataRestJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akash.springdatarestjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestJpaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/springdatarestjpa/SpringDataRestJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.springdatarestjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
